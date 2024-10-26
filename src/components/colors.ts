f82f769747efa9f40c150ed25f49ffacc5da7abb
norv1 <kelven.fellipe1@outlook.com>
2024-10-26 16:05:02 -0300

    feat: colors function

---
+++ src/components/colors.ts
@@ -0,0 +1,12 @@
+export function colors(material: string){
+ if (material === "Noble gases") return "bg-blue-400"
+ if (material === "Transition metals") return "bg-indigo-600"
+ if (material === "Alkali metals") return "bg-teal-800"
+ if (material === "Alkaline earth metals") return "bg-red-600"
+ if (material === "Reactive non-metals") return "bg-blue-600"
+ if (material === "Metalloids") return "bg-yellow-600"
+ if (material === "Post-transition metals") return "bg-green-600"
+ if (material === "Actinides") return "bg-orange-600"
+ if (material === "Lanthanides") return "bg-violet-600"
+ if (material === "Unknown properties") return "bg-zinc-600"
+}
