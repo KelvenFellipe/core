cc7a378b3e0f9269966affb8c4fa8d04f6931d12
norv1 <kelven.fellipe1@outlook.com>
2024-10-26 16:05:32 -0300

    feat: updated home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-900`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,87 @@
 import { Element } from "@/components/Element"
+import { elementsData } from "@/components/elements"
 
 export default function Home() {
   return (
-    <div>
-      <Element />
+    <div className="grid grid-cols-18 w-screen p-10 gap-1">
+      <div className="flex col-span-1">
+        <Element data={elementsData[0]} />
+      </div>
+      <div className="flex col-start-18">
+        <Element data={elementsData[1]} />
+      </div>
+
+      {elementsData
+        .filter(data => data.atomic > 2 && data.atomic < 5)
+        .map(data => (
+          <div className="col-span-1 flex">
+            <Element data={data} />
+          </div>
+        ))}
+      <div className="col-span-10"></div>
+      {elementsData
+        .filter(data => data.atomic > 4 && data.atomic < 11)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+
+      {elementsData
+        .filter(data => data.atomic > 10 && data.atomic < 13)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+      <div className="col-span-10"></div>
+
+      {elementsData
+        .filter(data => data.atomic > 12 && data.atomic < 19)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+
+      {elementsData
+        .filter(data => data.atomic > 18 && data.atomic < 58)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+      {elementsData
+        .filter(data => data.atomic > 71 && data.atomic < 90)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+      {elementsData
+        .filter(data => data.atomic > 103 && data.atomic < 119)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+      <div className="flex col-span-18 p-4"></div>
+      <div className="flex col-span-3 mt-2"></div>
+      {elementsData
+        .filter(data => data.atomic > 57 && data.atomic < 72)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
+      <div className="flex col-span-3 bg-"></div>
+      {elementsData
+        .filter(data => data.atomic > 89 && data.atomic < 104)
+        .map(data => (
+          <div className="flex col-span-1">
+            <Element data={data} />
+          </div>
+        ))}
     </div>
   )
 }
