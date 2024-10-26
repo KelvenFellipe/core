80ff3ab59dfc4f2f1a7d15cbb60c0259f1bd5ec7
norv1 <kelven.fellipe1@outlook.com>
2024-10-25 21:30:47 -0300

    feat: created Element component

---
+++ src/components/Element.tsx
@@ -0,0 +1,14 @@
+export function Element() {
+  return (
+    <div className="m-10">
+      <div className="aspect-square max-w-[120px] bg-cyan-500 rounded-md font-bold grid grid-cols-1 text-center p-1">
+        <div className="text-sm flex justify-between">
+          <p>1</p>
+          <p></p>
+        </div>
+        <p className="text-6xl">H</p>
+        <p className="text-sm">Hydrogen</p>
+      </div>
+    </div>
+  )
+}
