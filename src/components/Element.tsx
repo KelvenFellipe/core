1f2f93716a181eaa1a6257e644554f1052b28fff
norv1 <kelven.fellipe1@outlook.com>
2024-10-26 16:05:20 -0300

    feat: updated Element component

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,14 +1,31 @@
-export function Element() {
+"use client"
+import { useState } from "react"
+import { colors } from "./colors"
+import { elementsProps } from "./elements"
+
+interface props {
+  data: elementsProps
+  className?: string
+}
+export function Element({ data, className }: props) {
+  const [hover, setHover] = useState(false)
+
   return (
-    <div className="m-10">
-      <div className="aspect-square max-w-[120px] bg-cyan-500 rounded-md font-bold grid grid-cols-1 text-center p-1">
-        <div className="text-sm flex justify-between">
-          <p>1</p>
-          <p></p>
-        </div>
-        <p className="text-6xl">H</p>
-        <p className="text-sm">Hydrogen</p>
+    <div
+      className={`relative aspect-square w-[92px] rounded-md font-bold grid grid-cols-1 text-center p-1 
+        ease-in-out duration-300 select-none border-black ${colors(data.material)} ${className} ${
+        hover && "scale-125 z-40 shadow-2xl shadow-black border "
+      }`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      key={data.atomic}
+    >
+      <div className="text-xs flex justify-between ">
+        <p>{data.atomic}</p>
+        <p className={`${hover ? "" : "hidden"}`}>{data.mass}</p>
       </div>
+      <p className="text-4xl">{data.abv}</p>
+      <p className="text-sm scale-75">{data.name}</p>
     </div>
   )
 }
